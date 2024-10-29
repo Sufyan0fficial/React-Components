4e54fe356fe6ae18a4f45bf67d63a5d7fa9a3e85
Muhammad Sufyan <computer388unofficial@gmail.com>
2024-10-29 18:58:04 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-import ComplexImgComp from "./components/XevenSolutions/ComplexImgComp";
-import ComplexComp2 from "./components/XevenSolutions/ComplexComp2";
-import HealthTech from "./components/XevenSolutions/HealthTech";
-import SwiperCarousel from "./components/XevenSolutions/SwiperCarousel";
 import AboutUS from "./components/XevenSolutions/AboutUS";
 import HeaderComponent from "./components/XevenSolutions/header";
 
